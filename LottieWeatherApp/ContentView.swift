2bc3f78a13fa6b03a8fa3ae5938498170f66d59a
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-05-07 20:57:36 +0300

    Initial Commit

---
+++ LottieWeatherApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LottieWeatherApp
+//
+//  Created by ramil on 07.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LottieWeatherApp/LottieWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieWeatherAppApp.swift
+//  LottieWeatherApp
+//
+//  Created by ramil on 07.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct LottieWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
