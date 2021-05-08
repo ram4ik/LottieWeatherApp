517ff632559553957bd533aaa1ce8b46e549a24d
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-05-08 21:39:01 +0300

    lottie animation

---
+++ LottieWeatherApp/Views/LottieView.swift
@@ -0,0 +1,37 @@
+//
+//  LottieView.swift
+//  LottieWeatherApp
+//
+//  Created by ramil on 08.05.2021.
+//
+
+import SwiftUI
+import Lottie
+
+struct LottieView: UIViewRepresentable {
+    var name: String
+    var loopMode: LottieLoopMode = .loop
+    
+    func makeUIView(context: UIViewRepresentableContext<LottieView>) -> some UIView {
+        let view = UIView()
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        uiView.subviews.forEach({$0.removeFromSuperview()})
+        
+        let animationView = AnimationView()
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        uiView.addSubview(animationView)
+        
+        NSLayoutConstraint.activate([
+            animationView.widthAnchor.constraint(equalTo: uiView.widthAnchor),
+            animationView.heightAnchor.constraint(equalTo: uiView.heightAnchor)
+        ])
+        
+        animationView.animation = Animation.named(name)
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = loopMode
+        animationView.play()
+    }
+}
